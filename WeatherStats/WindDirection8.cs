861c547c9094371c85e021c74dc42811c4427383
Julien <julien.etelain@outlook.com>
2023-09-24 12:06:21 +0200

    Comments to avoid confusion

--- WeatherStats/WindDirection8.cs
+++ WeatherStats/WindDirection8.cs
@@ -1,14 +1,41 @@
 ﻿namespace WeatherStats
 {
+    /// <summary>
+    /// Wind direction (it's the opposite of his origin)
+    /// </summary>
     public enum WindDirection8
     {
+        /// <summary>
+        /// Wind going North (coming from South)
+        /// </summary>
         North,
+        /// <summary>
+        /// Wind going North West (coming from South East)
+        /// </summary>
         NorthWest,
+        /// <summary>
+        /// Wind going West (coming from East)
+        /// </summary>
         West,
+        /// <summary>
+        /// Wind going South West (coming from North East)
+        /// </summary>
         SouthWest,
+        /// <summary>
+        /// Wind going South (coming from North)
+        /// </summary>
         South,
+        /// <summary>
+        /// Wind going South East (coming from North West)
+        /// </summary>
         SouthEast,
+        /// <summary>
+        /// Wind going East (coming from West)
+        /// </summary>
         East,
+        /// <summary>
+        /// Wind going North East (coming from South West)
+        /// </summary>
         NorthEast
     }
 }
