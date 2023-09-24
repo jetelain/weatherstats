63b9aced5ea72092baaa4197c25ce3418a2daad0
Julien <julien.etelain@outlook.com>
2023-09-24 11:41:45 +0200

    CDN needs a user agent

--- WeatherStats/Databases/WeatherStatsHttpStorage.cs
+++ WeatherStats/Databases/WeatherStatsHttpStorage.cs
@@ -10,6 +10,7 @@ namespace WeatherStats.Databases
         public WeatherStatsHttpStorage(HttpClient client)
         {
             this.client = client;
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0");
         }
 
         public WeatherStatsHttpStorage(Uri baseAddress)
